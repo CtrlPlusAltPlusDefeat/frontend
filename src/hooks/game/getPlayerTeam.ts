e9ca28d5990fd7cce3743782bf82d29be8a71f1e
Env-byte <46604415+Env-byte@users.noreply.github.com>
2023-07-03 16:08:15 +0100

    New Team Response

--- src/hooks/game/getPlayerTeam.ts
+++ src/hooks/game/getPlayerTeam.ts
@@ -4,5 +4,5 @@ import { useGameStore } from '../../stores/game/gameStore';
 export const useGetPlayerTeam = () => {
 	const player = useLobbyStore((s) => s.player);
 	const teams = useGameStore((s) => s.teams);
-	return teams.find((t) => t.players.includes(player?.id ?? ''));
+	return teams.find((t) => t.players.find((p) => p.id === player?.id ?? ''));
 };
